85b0c2c308c42ea3ff6a5ce139e4b18de26907ab
Germain Leignel <germain.leignel@gmail.com>
2023-04-07 10:39:51 +0200

    update

--- create_tables.sql
+++ create_tables.sql
@@ -1,52 +1,65 @@
 CREATE TABLE users(
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(50) UNIQUE,
-    first_name TEXT,
-    last_name TEXT,
-    email TEXT UNIQUE
+   id SERIAL PRIMARY KEY,
+   username VARCHAR(50) UNIQUE,
+   firstname TEXT,
+   lastname TEXT,
+   email TEXT UNIQUE
 );
 
-CREATE TABLE species(id SERIAL PRIMARY KEY, name TEXT);
+CREATE TABLE species(
+   id SERIAL PRIMARY KEY,
+   name TEXT
+);
 
 CREATE TABLE advice(
-    id SERIAL PRIMARY KEY,
-    text TEXT,
-    species_id INT NOT NULL,
-    FOREIGN KEY (species_id) REFERENCES species(id)
+   id SERIAL PRIMARY KEY,
+   text TEXT,
+   species_id INT NOT NULL REFERENCES species(id)
 );
 
 CREATE TABLE location(
-    id SERIAL PRIMARY KEY,
-    address VARCHAR(255),
-    zip VARCHAR(5),
-    city TEXT,
-    lat DECIMAL(15, 2),
-    lon DECIMAL(15, 2)
+   id SERIAL PRIMARY KEY,
+   address VARCHAR(255),
+   zip VARCHAR(5),
+   city TEXT,
+   lat DECIMAL(15,2),
+   lon DECIMAL(15,2)
 );
 
 CREATE TABLE plants(
-    id SERIAL PRIMARY KEY,
-    location_id INT NOT NULL,
-    species_id INT NOT NULL,
-    user_id INT NOT NULL,
-    FOREIGN KEY (location_id) REFERENCES location(id),
-    FOREIGN KEY (species_id) REFERENCES species(id),
-    FOREIGN KEY (user_id) REFERENCES users(id)
+   id SERIAL PRIMARY KEY,
+   location_id INT NOT NULL REFERENCES location(id),
+   species_id INT NOT NULL REFERENCES species(id),
+   user_id INT NOT NULL REFERENCES users(id)
+);
+
+CREATE TABLE photo(
+   id SERIAL PRIMARY KEY,
+   url TEXT,
+   plant_id INT NOT NULL REFERENCES plants(id)
 );
 
 CREATE TABLE keeps(
-    user_id INT,
-    location_id INT,
-    PRIMARY KEY (user_id, location_id),
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (location_id) REFERENCES location(id)
+   id SERIAL PRIMARY KEY,
+   start_date DATE,
+   end_date DATE,
+   plant_id INT NOT NULL REFERENCES plants(id),
+   user_id INT NOT NULL REFERENCES users(id),
+   location_id INT NOT NULL REFERENCES location(id)
 );
 
 CREATE TABLE message(
-    sender_id INT,
-    recipient_id INT,
-    text VARCHAR(50),
-    PRIMARY KEY (sender_id, recipient_id),
-    FOREIGN KEY (sender_id) REFERENCES users(id),
-    FOREIGN KEY (recipient_id) REFERENCES users(id)
+   id SERIAL,
+   sender_id INT NOT NULL REFERENCES users(id),
+   receiver_id INT NOT NULL REFERENCES users(id),
+   text VARCHAR(50),
+   PRIMARY KEY(id, sender_id, receiver_id)
+);
+
+CREATE TABLE need_keep(
+   user_id INT NOT NULL REFERENCES users(id),
+   start_date DATE,
+   end_date DATE,
+   plant_id INT NOT NULL REFERENCES plants(id),
+   PRIMARY KEY(user_id)
 );
