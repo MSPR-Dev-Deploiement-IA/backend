213c74feff6ed3cc227cdb4d4cfe6f0c72eec926
Germain-L <germain.leignel@gmail.com>
2023-04-26 14:29:15 +0200

    database

--- database/setup.go
+++ database/setup.go
@@ -3,6 +3,7 @@ package database
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -10,17 +11,25 @@ import (
 
 func SetupDatabase() (*gorm.DB, error) {
 	host := os.Getenv("DB_HOST")
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	dbname := os.Getenv("DB_NAME")
+	user := os.Getenv("POSTGRES_USER")
+	password := os.Getenv("POSTGRES_PASSWORD")
+	dbname := os.Getenv("POSTGRES_DB")
 	port := os.Getenv("DB_PORT")
 
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, dbname, port)
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		return nil, fmt.Errorf("failed to connect to database: %w", err)
+	var db *gorm.DB
+	var err error
+
+	for i := 0; i < 10; i++ {
+		db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		if err == nil {
+			return db, nil
+		}
+
+		fmt.Printf("Failed to connect to database. Retrying in %d seconds...\n", i*5)
+		time.Sleep(time.Duration(i*5) * time.Second)
 	}
 
-	return db, nil
+	return nil, fmt.Errorf("failed to connect to database after 10 attempts: %w", err)
 }
