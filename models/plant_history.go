df1de4a7a21d3e7a6787fa2e2f23c9b05de6cff5
Germain <germain.leignel@gmail.com>
2023-05-16 15:46:06 +0200

    history

--- models/plant_history.go
+++ models/plant_history.go
@@ -21,3 +21,46 @@ func (p *PlantHistory) Save(db *gorm.DB) error {
 	}
 	return nil
 }
+
+type PlantHistoryRepository struct {
+	db *gorm.DB
+}
+
+func NewPlantHistoryRepository(db *gorm.DB) *PlantHistoryRepository {
+	return &PlantHistoryRepository{db: db}
+}
+
+// Create a new plant history
+func (r *PlantHistoryRepository) Create(plantHistory *PlantHistory) error {
+	return r.db.Create(plantHistory).Error
+}
+
+// Get a plant history by ID
+func (r *PlantHistoryRepository) GetByID(id uint) (*PlantHistory, error) {
+	var plantHistory PlantHistory
+	err := r.db.First(&plantHistory, id).Error
+	if err != nil {
+		return nil, err
+	}
+	return &plantHistory, nil
+}
+
+// Update a plant history
+func (r *PlantHistoryRepository) Update(plantHistory *PlantHistory) error {
+	return r.db.Save(plantHistory).Error
+}
+
+// Delete a plant history
+func (r *PlantHistoryRepository) Delete(plantHistory *PlantHistory) error {
+	return r.db.Delete(plantHistory).Error
+}
+
+// Get all plant histories for a plant
+func (r *PlantHistoryRepository) GetByPlantID(plantID uint) ([]PlantHistory, error) {
+	var plantHistories []PlantHistory
+	err := r.db.Where("plant_id = ?", plantID).Find(&plantHistories).Error
+	if err != nil {
+		return nil, err
+	}
+	return plantHistories, nil
+}
