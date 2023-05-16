5978d5d9093952cc4e4e8d7faca9f741c96859ed
Germain <germain.leignel@gmail.com>
2023-05-16 15:44:56 +0200

    added plant repo

--- models/plant.go
+++ models/plant.go
@@ -23,3 +23,46 @@ func (p *Plant) Save(db *gorm.DB) error {
 	}
 	return nil
 }
+
+type PlantRepository struct {
+	db *gorm.DB
+}
+
+func NewPlantRepository(db *gorm.DB) *PlantRepository {
+	return &PlantRepository{db: db}
+}
+
+// Create a new plant
+func (r *PlantRepository) Create(plant *Plant) error {
+	return r.db.Create(plant).Error
+}
+
+// Get a plant by ID
+func (r *PlantRepository) GetByID(id uint) (*Plant, error) {
+	var plant Plant
+	err := r.db.Preload("PlantHistories").Preload("PlantAdvices").Preload("Photos").First(&plant, id).Error
+	if err != nil {
+		return nil, err
+	}
+	return &plant, nil
+}
+
+// Update a plant
+func (r *PlantRepository) Update(plant *Plant) error {
+	return r.db.Save(plant).Error
+}
+
+// Delete a plant
+func (r *PlantRepository) Delete(plant *Plant) error {
+	return r.db.Delete(plant).Error
+}
+
+// Get all plants for a user
+func (r *PlantRepository) GetByUserID(userID uint) ([]Plant, error) {
+	var plants []Plant
+	err := r.db.Where("user_id = ?", userID).Find(&plants).Error
+	if err != nil {
+		return nil, err
+	}
+	return plants, nil
+}
