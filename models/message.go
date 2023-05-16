f46cf683e968f3d494394432ef0bed6bc3946b81
Germain <germain.leignel@gmail.com>
2023-05-16 15:51:07 +0200

    models

--- models/message.go
+++ models/message.go
@@ -13,3 +13,46 @@ type Message struct {
 	MessageText string    `gorm:"type:varchar(255);not null"`
 	Timestamp   time.Time `gorm:"not null"`
 }
+
+type MessageRepository struct {
+	db *gorm.DB
+}
+
+func NewMessageRepository(db *gorm.DB) *MessageRepository {
+	return &MessageRepository{db: db}
+}
+
+// Create a new message
+func (r *MessageRepository) Create(message *Message) error {
+	return r.db.Create(message).Error
+}
+
+// Get a message by ID
+func (r *MessageRepository) GetByID(id uint) (*Message, error) {
+	var message Message
+	err := r.db.First(&message, id).Error
+	if err != nil {
+		return nil, err
+	}
+	return &message, nil
+}
+
+// Update a message
+func (r *MessageRepository) Update(message *Message) error {
+	return r.db.Save(message).Error
+}
+
+// Delete a message
+func (r *MessageRepository) Delete(message *Message) error {
+	return r.db.Delete(message).Error
+}
+
+// Get all messages between two users
+func (r *MessageRepository) GetByUsers(senderID uint, receiverID uint) ([]Message, error) {
+	var messages []Message
+	err := r.db.Where("(sender_id = ? AND receiver_id = ?) OR (sender_id = ? AND receiver_id = ?)", senderID, receiverID, receiverID, senderID).Order("timestamp asc").Find(&messages).Error
+	if err != nil {
+		return nil, err
+	}
+	return messages, nil
+}
--- models/plant_advice.go
+++ models/plant_advice.go
@@ -10,3 +10,46 @@ type PlantAdvice struct {
 	UserID     uint   `gorm:"not null"`
 	AdviceText string `gorm:"type:varchar(255);not null"`
 }
+
+type PlantAdviceRepository struct {
+	db *gorm.DB
+}
+
+func NewPlantAdviceRepository(db *gorm.DB) *PlantAdviceRepository {
+	return &PlantAdviceRepository{db: db}
+}
+
+// Create a new plant advice
+func (r *PlantAdviceRepository) Create(plantAdvice *PlantAdvice) error {
+	return r.db.Create(plantAdvice).Error
+}
+
+// Get a plant advice by ID
+func (r *PlantAdviceRepository) GetByID(id uint) (*PlantAdvice, error) {
+	var plantAdvice PlantAdvice
+	err := r.db.First(&plantAdvice, id).Error
+	if err != nil {
+		return nil, err
+	}
+	return &plantAdvice, nil
+}
+
+// Update a plant advice
+func (r *PlantAdviceRepository) Update(plantAdvice *PlantAdvice) error {
+	return r.db.Save(plantAdvice).Error
+}
+
+// Delete a plant advice
+func (r *PlantAdviceRepository) Delete(plantAdvice *PlantAdvice) error {
+	return r.db.Delete(plantAdvice).Error
+}
+
+// Get all plant advices for a plant
+func (r *PlantAdviceRepository) GetByPlantID(plantID uint) ([]PlantAdvice, error) {
+	var plantAdvices []PlantAdvice
+	err := r.db.Where("plant_id = ?", plantID).Find(&plantAdvices).Error
+	if err != nil {
+		return nil, err
+	}
+	return plantAdvices, nil
+}
